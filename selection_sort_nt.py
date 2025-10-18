f3b61d6924b4611f0653717e12ce31bdde1624ec
ntalc <albuquerquefrazao@hotmail.com>
2025-10-18 20:30:30 -0300

    Tentei implementar o selection sort mais simples

--- selection_sort_nt.py
+++ selection_sort_nt.py
@@ -15,3 +15,20 @@ for nome_coluna in df.columns:
     print(listas_para_analise)
 
 
+
+    def selection_sort(lista):
+        n = len(lista)
+        for i in range(n):
+            indice_menor + i
+           
+            for j in range(i + 1,n):
+                if lista[j] < lista[indice_menor]:
+                    indice_menor = j
+            
+            lista[i], lista[indice_menor] = lista[indice_menor], lista[i]
+
+        return lista
+
+numeros = [0,100]
+print("Lista ordenada com Selection Sort: ", selection_sort(numeros))
+
