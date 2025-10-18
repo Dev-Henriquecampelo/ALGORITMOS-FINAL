8eb281bed413622c87086d0e6b0d7e7bbefd6049
paullohfs <paullohfs@hotmail.com>
2025-10-18 20:22:56 -0300

    Tentativa implementação Selection Sort

--- selection_sort_pf.py
+++ selection_sort_pf.py
@@ -12,4 +12,20 @@ for nome_coluna in df.columns:
 
 # Agora, 'listas_para_analise' é um dicionário com 4 listas prontas para o teste.
 
-print(listas_para_analise)
+#print(listas_para_analise)
+
+def selection_sort(lista):
+    n = len(lista)
+    for i in range(n):
+        indice_menor = i
+
+        for j in range(i + 1, n):
+            if lista[j] < lista[indice_menor]:
+                indice_menor = j
+
+        lista[i], lista[indice_menor] = lista[indice_menor], lista[i]
+
+    return lista
+
+numeros = [64, 34, 25, 12, 22, 11, 90]
+print("Lista ordenada com Selection Sort:", selection_sort(numeros))
