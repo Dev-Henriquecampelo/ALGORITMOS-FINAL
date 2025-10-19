675bd49c34e4a48d7f3b593519d3fca4a5862827
paullohfs <paullohfs@hotmail.com>
2025-10-19 13:37:38 -0300

    Converti meu Selection Sort de lista para array

--- selection_sort_pf.py
+++ selection_sort_pf.py
@@ -14,21 +14,34 @@ listas_para_analise = {
 
 # Agora, 'listas_para_analise' é um dicionário com 4 listas prontas para o teste.
 
-def selection_sort(lista):
-    n = len(lista)
+# def selection_sort(lista):
+#     n = len(lista)
+#     for i in range(n):
+#         indice_menor = i
+
+#         for j in range(i + 1, n):
+#             if lista[j] < lista[indice_menor]:
+#                 indice_menor = j
+
+#         lista[i], lista[indice_menor] = lista[indice_menor], lista[i]
+
+#     return lista
+
+def selection_sort(array):
+    n = len(array)
     for i in range(n):
         indice_menor = i
 
         for j in range(i + 1, n):
-            if lista[j] < lista[indice_menor]:
+            if array[j] < array[indice_menor]:
                 indice_menor = j
 
-        lista[i], lista[indice_menor] = lista[indice_menor], lista[i]
+        array[i], array[indice_menor] = array[indice_menor], array[i]
 
-    return lista
+    return array
 
 #numeros = listas_para_analise[nome_coluna]
 # print("Lista ordenada com Selection Sort:", selection_sort(listas_para_analise["pequena_100"]))
 # print("Lista ordenada com Selection Sort:", selection_sort(listas_para_analise["media_1000"]))
 # print("Lista ordenada com Selection Sort:", selection_sort(listas_para_analise["grande_10000"]))
-print("Lista ordenada com Selection Sort:", selection_sort(listas_para_analise["muito_grande_50000"]))
+# print("Lista ordenada com Selection Sort:", selection_sort(listas_para_analise["muito_grande_50000"]))
